25d218bcf4cc9fc7c6fb9bb7862418e76fbde426
= <mukhtarrahimi.dev@gmail.com>
2025-10-17 11:20:48 +0430

    template

---
+++ components/templates/Menu/Prising.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Prising() {
+  return <div>Prising</div>;
+}
+
+export default Prising;
---
+++ components/templates/Reservation/ReservationDetails.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ReservationDetails() {
+  return <div>ReservationDetails</div>;
+}
+
+export default ReservationDetails;
