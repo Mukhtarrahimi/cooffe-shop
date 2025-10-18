382b4033fe14bd52af71b6472d56e95ebe9a748f
= <mukhtarrahimi.dev@gmail.com>
2025-10-18 09:07:04 +0430

    loop at links

--- components/modules/Navbar/Navbar.js
+++ components/modules/Navbar/Navbar.js
@@ -5,6 +5,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
+  const links = [
+    { name: 'Home', path: '/' },
+    { name: 'About', path: '/about' },
+    { name: 'Service', path: '/services' },
+    { name: 'Menu', path: '/menu' },
+    {
+      name: 'Pages',
+      dropdown: [
+        { name: 'Reservation', path: '/reservation' },
+        { name: 'Testimonial', path: '/testimonial' },
+      ],
+    },
+    { name: 'Contact', path: '/contact' },
+  ];
+
   return (
     <div>
       <nav>
@@ -16,22 +31,11 @@ function Navbar() {
         </div>
         <div className="bars">
           <ul>
-            {
-              (links = [
-                { name: 'Home', path: '/' },
-                { name: 'About', path: '/about' },
-                { name: 'Service', path: '/services' },
-                { name: 'Menu', path: '/menu' },
-                {
-                  name: 'Pages',
-                  dropdown: [
-                    { name: 'Reservation', path: '/reservation' },
-                    { name: 'Testimonial', path: '/testimonial' },
-                  ],
-                },
-                { name: 'Contact', path: '/contact' },
-              ])
-            }
+            {links.map((link, index) => (
+              <li key={index}>
+                <Link href={link.path || '#'}>{link.name}</Link>
+              </li>
+            ))}
           </ul>
         </div>
       </nav>
