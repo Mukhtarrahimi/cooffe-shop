ebd4ee92bec28b9b464761c5b5c782e4b28be330
= <mukhtarrahimi.dev@gmail.com>
2025-10-19 19:38:58 +0430

    add swiper code from site

--- components/templates/index/Slider.js
+++ components/templates/index/Slider.js
@@ -6,13 +6,18 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
-import './styles.css';
-
 // import required modules
 import { Navigation } from 'swiper/modules';
 
 function Slider() {
-  return <div>Slider</div>;
+  return (
+    <>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
 
 export default Slider;
