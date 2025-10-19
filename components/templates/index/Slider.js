6103f05625b3f9c24f5ba75de291e791e590a344
= <mukhtarrahimi.dev@gmail.com>
2025-10-19 19:37:35 +0430

    link import

--- components/templates/index/Slider.js
+++ components/templates/index/Slider.js
@@ -1,4 +1,15 @@
 import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import './styles.css';
+
+// import required modules
+import { Navigation } from 'swiper/modules';
 
 function Slider() {
   return <div>Slider</div>;
