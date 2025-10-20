9ff5c7a852656c939411e1e06b2803f63603cf4a
= <mukhtarrahimi.dev@gmail.com>
2025-10-20 20:53:30 +0430

    about page import to index.js file

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
+import About from '@/components/templates/Index/About';
 import Slider from '@/components/templates/Index/Slider';
 import React from 'react';
 
 function index() {
-  return <Slider />;
+  return (
+    <>
+      <Slider />
+      <About />
+    </>
+  );
 }
 
 export default index;
