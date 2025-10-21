c859c2bd0a2591abb9a2f0e975033777f779bb73
= <mukhtarrahimi.dev@gmail.com>
2025-10-21 09:03:52 +0430

    code update

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import About from '@/components/templates/Index/About';
-import Services from '@/components/templates/Index/Services';
 import Slider from '@/components/templates/Index/Slider';
 import React from 'react';
+import About from '@/components/templates/Index/About';
+import Services from '@/components/templates/Index/Services';
 
-function index({ data }) {
+function Index({ data }) {
   return (
     <>
       <Slider />
@@ -16,14 +16,15 @@ function index({ data }) {
 export async function getStaticProps() {
   const res = await fetch('http://localhost:4000/services');
   const services = await res.json();
+
   return {
     props: {
       data: {
         services,
       },
     },
-    revalidate: 60 * 60 * 12,
+    revalidate: 60 * 60 * 12, // Second
   };
 }
 
-export default index;
+export default Index;
