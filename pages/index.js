05f75554fb270309149e3576407e15086e14895c
= <mukhtarrahimi.dev@gmail.com>
2025-10-22 21:35:17 +0430

    code update

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import About from '@/components/templates/Index/About';
 import Services from '@/components/templates/Index/Services';
 import Offer from '@/components/templates/Index/Offer';
 import Menu from '@/components/templates/Index/Menu';
+import Reservation from '@/components/templates/Index/reservation';
 
 function Index({ data }) {
   return (
@@ -12,19 +13,24 @@ function Index({ data }) {
       <About />
       <Services services={data.services} />
       <Offer />
-      <Menu />
+      <Menu data={data.menu} />
+      <Reservation />
     </>
   );
 }
 
 export async function getStaticProps() {
-  const res = await fetch('http://localhost:4000/services');
-  const services = await res.json();
+  const servicesResponse = await fetch('http://localhost:4000/services');
+  const servicesData = await servicesResponse.json();
+
+  const menuResponse = await fetch('http://localhost:4000/menu');
+  const menuData = await menuResponse.json();
 
   return {
     props: {
       data: {
-        services,
+        services: servicesData,
+        menu: menuData,
       },
     },
     revalidate: 60 * 60 * 12, // Second
