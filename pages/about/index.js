41637e26f9a52c9b71aa29421b7d6d9919747b55
= <mukhtarrahimi.dev@gmail.com>
2025-10-25 09:03:14 +0430

    pageHeader & story add to about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,7 +1,14 @@
+import PageHeader from '@/components/modules/PageHeader/PageHeader';
+import Story from '@/components/templates/About/Story';
 import React from 'react';
 
 function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <PageHeader />
+      <Story />
+    </>
+  );
 }
 
 export default About;
