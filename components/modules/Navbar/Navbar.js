a3e56ec187e9b757d56fb22e55edf01f425fc484
= <mukhtarrahimi.dev@gmail.com>
2025-10-26 09:21:02 +0430

    navbar code update

--- components/modules/Navbar/Navbar.js
+++ components/modules/Navbar/Navbar.js
@@ -39,10 +39,10 @@ function Navbar() {
             <Link href="/services" className={`${styles.nav_link}`}>
               Services
             </Link>
-
-            <a href="menu.html" className={`${styles.nav_link}`}>
+            <Link href="/menu" className={`${styles.nav_link}`}>
               Menu
-            </a>
+            </Link>
+
             <div className={`${styles.dropdown}`}>
               <a
                 href="#"
