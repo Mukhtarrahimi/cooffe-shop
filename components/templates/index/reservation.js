05932297c7da49f2d291510190fcb0be64a60aaf
= <mukhtarrahimi.dev@gmail.com>
2025-10-27 13:30:15 +0430

    reservation index

--- components/templates/index/reservation.js
+++ components/templates/index/reservation.js
@@ -1,127 +1,99 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function Reservation() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [time, setTime] = useState('');
-  const [person, setPerson] = useState('');
-
-  const infoHandler = async (event) => {
-    event.preventDefault(); // prevent form reload
-
-    try {
-      const response = await fetch('http://localhost:4000/reserve', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name, email, time, person }),
-      });
-
-      if (response.status === 201) {
-        setName('');
-        setEmail('');
-        setTime('');
-        setPerson('');
-        alert('Reservation Successful 🎉');
-      } else {
-        alert('Something went wrong. Please try again.');
-      }
-    } catch (error) {
-      console.error('Error submitting reservation:', error);
-      alert('Server error. Please try again later.');
-    }
-  };
-
   return (
-    <div className="container-fluid my-5">
-      <div className="container">
-        <div className="reservation position-relative overlay-top overlay-bottom">
-          <div className="row align-items-center">
-            <div className="col-lg-6 my-5 my-lg-0">
-              <div className="p-5">
-                <div className="mb-4">
-                  <h1 className="display-3 text-primary">30% OFF</h1>
-                  <h1 className="text-white">For Online Reservation</h1>
+    <div class="container-fluid my-5">
+      <div class="container">
+        <div class="reservation position-relative overlay-top overlay-bottom">
+          <div class="row align-items-center">
+            <div class="col-lg-6 my-5 my-lg-0">
+              <div class="p-5">
+                <div class="mb-4">
+                  <h1 class="display-3 text-primary">30% OFF</h1>
+                  <h1 class="text-white">For Online Reservation</h1>
                 </div>
-                <p className="text-white">
+                <p class="text-white">
                   Lorem justo clita erat lorem labore ea, justo dolor lorem
                   ipsum ut sed eos, ipsum et dolor kasd sit ea justo. Erat justo
                   sed sed diam. Ea et erat ut sed diam sea
                 </p>
-                <ul className="list-inline text-white m-0">
-                  <li className="py-2">
-                    <i className="fa fa-check text-primary mr-3"></i>Lorem ipsum
+                <ul class="list-inline text-white m-0">
+                  <li class="py-2">
+                    <i class="fa fa-check text-primary mr-3"></i>Lorem ipsum
                     dolor sit amet
                   </li>
-                  <li className="py-2">
-                    <i className="fa fa-check text-primary mr-3"></i>Lorem ipsum
+                  <li class="py-2">
+                    <i class="fa fa-check text-primary mr-3"></i>Lorem ipsum
                     dolor sit amet
                   </li>
-                  <li className="py-2">
-                    <i className="fa fa-check text-primary mr-3"></i>Lorem ipsum
+                  <li class="py-2">
+                    <i class="fa fa-check text-primary mr-3"></i>Lorem ipsum
                     dolor sit amet
                   </li>
                 </ul>
               </div>
             </div>
-
-            <div className="col-lg-6">
+            <div class="col-lg-6">
               <div
-                className="text-center p-5"
+                class="text-center p-5"
                 style={{ background: 'rgba(51, 33, 29, .8)' }}
               >
-                <h1 className="text-white mb-4 mt-5">Book Your Table</h1>
-                <form className="mb-5" onSubmit={infoHandler}>
-                  <div className="form-group">
+                <h1 class="text-white mb-4 mt-5">Book Your Table</h1>
+                <form class="mb-5">
+                  <div class="form-group">
                     <input
                       type="text"
-                      className="form-control bg-transparent border-primary p-4"
+                      class="form-control bg-transparent border-primary p-4"
                       placeholder="Name"
-                      required
-                      value={name}
-                      onChange={(e) => setName(e.target.value)}
+                      required="required"
                     />
                   </div>
-                  <div className="form-group">
+                  <div class="form-group">
                     <input
                       type="email"
-                      className="form-control bg-transparent border-primary p-4"
+                      class="form-control bg-transparent border-primary p-4"
                       placeholder="Email"
-                      required
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
+                      required="required"
                     />
                   </div>
-                  <div className="form-group">
-                    <input
-                      type="text"
-                      className="form-control bg-transparent border-primary p-4"
-                      placeholder="Time"
-                      required
-                      value={time}
-                      onChange={(e) => setTime(e.target.value)}
-                    />
+                  <div class="form-group">
+                    <div class="date" id="date" data-target-input="nearest">
+                      <input
+                        type="text"
+                        class="form-control bg-transparent border-primary p-4 datetimepicker-input"
+                        placeholder="Date"
+                        data-target="#date"
+                        data-toggle="datetimepicker"
+                      />
+                    </div>
                   </div>
-                  <div className="form-group">
+                  <div class="form-group">
+                    <div class="time" id="time" data-target-input="nearest">
+                      <input
+                        type="text"
+                        class="form-control bg-transparent border-primary p-4 datetimepicker-input"
+                        placeholder="Time"
+                        data-target="#time"
+                        data-toggle="datetimepicker"
+                      />
+                    </div>
+                  </div>
+                  <div class="form-group">
                     <select
-                      className="custom-select bg-transparent border-primary px-4"
+                      class="custom-select bg-transparent border-primary px-4"
                       style={{ height: '49px' }}
-                      required
-                      value={person}
-                      onChange={(e) => setPerson(e.target.value)}
                     >
-                      <option value="">Person</option>
+                      <option selected="">Person</option>
                       <option value="1">Person 1</option>
                       <option value="2">Person 2</option>
                       <option value="3">Person 3</option>
-                      <option value="4">Person 4</option>
+                      <option value="3">Person 4</option>
                     </select>
                   </div>
 
                   <div>
                     <button
-                      className="btn btn-primary btn-block font-weight-bold py-3"
+                      class="btn btn-primary btn-block font-weight-bold py-3"
                       type="submit"
                     >
                       Book Now
