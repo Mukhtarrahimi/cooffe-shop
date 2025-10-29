bc7e373a37b93813fe8a8b030a817a1ddc30db37
= <mukhtarrahimi.dev@gmail.com>
2025-10-29 07:52:38 +0430

    getserversiteprops function

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,7 +1,27 @@
+import PageHeader from '@/components/modules/PageHeader/PageHeader';
 import React from 'react';
 
 function Search() {
-  return <div>Search</div>;
+  return (
+    <>
+      <PageHeader route="Search" />
+    </>
+  );
+}
+export async function getServerSiteProps(context) {
+  const { query } = context;
+  const result = await fetch('http://localhost:4000/menu');
+  const data = await result.json();
+  const resultSearh = data.filter(
+    (item) =>
+      item.type.toLowerCasse().includes(query.q.toLowerCasse()) ||
+      item.title.toLowerCasse().includes(query.q.toLowerCasse())
+  );
+  return {
+    props: {
+      data: resultSearh,
+    },
+  };
 }
 
 export default Search;
