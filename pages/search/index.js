4f2a59bb09b627664f7e9900e77b73bc4de1cc85
= <mukhtarrahimi.dev@gmail.com>
2025-10-29 07:54:29 +0430

    search page code complate

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,25 +1,31 @@
 import PageHeader from '@/components/modules/PageHeader/PageHeader';
+import Result from '@/components/templates/Search/Result';
 import React from 'react';
 
-function Search() {
+function Search({ data }) {
   return (
     <>
       <PageHeader route="Search" />
+      <Result searchResult={data} />
     </>
   );
 }
-export async function getServerSiteProps(context) {
+
+export async function getServerSideProps(context) {
   const { query } = context;
-  const result = await fetch('http://localhost:4000/menu');
-  const data = await result.json();
-  const resultSearh = data.filter(
+
+  const res = await fetch('http://localhost:4000/menu');
+  const data = await res.json();
+
+  const searchResult = data.filter(
     (item) =>
-      item.type.toLowerCasse().includes(query.q.toLowerCasse()) ||
-      item.title.toLowerCasse().includes(query.q.toLowerCasse())
+      item.type.toLowerCase().includes(query.q.toLowerCase()) ||
+      item.title.toLowerCase().includes(query.q.toLowerCase())
   );
+
   return {
     props: {
-      data: resultSearh,
+      data: searchResult,
     },
   };
 }
