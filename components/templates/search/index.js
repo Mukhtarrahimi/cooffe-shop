6c2438b3796902d4cb830589fc94c6d9e1256843
= <mukhtarrahimi.dev@gmail.com>
2025-10-29 08:12:56 +0430

    search index update

--- components/templates/search/index.js
+++ components/templates/search/index.js
@@ -1,30 +1,38 @@
-import Card from '@/components/modules/Card/Card';
 import React from 'react';
+import Card from '@/components/modules/Card/Card';
 
-function Result({ searchResult }) {
+function Result({ searchResult = [] }) {
+  const hotItems = searchResult
+    .filter((item) => item.type === 'hot')
+    .slice(0, 3);
+  const coldItems = searchResult
+    .filter((item) => item.type === 'cold')
+    .slice(0, 3);
+
   return (
-    <div class="container-fluid pt-5">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-6">
-            <h1 class="mb-5">Hot Coffee</h1>
-            {/*  */}
-            {searchResult
-              .filter((item) => item.type === 'hot')
-              .slice(0, 3)
-              .map((item) => (
-                <Card {...item} key={item.id} />
-              ))}
-          </div>
-          <div class="col-lg-6">
-            <h1 class="mb-5">Cold Coffee</h1>
+    <div className="container-fluid pt-5">
+      <div className="container">
+        <div className="row">
+          {/* --- Hot Coffee --- */}
+          <div className="col-lg-6">
+            <h1 className="mb-5">Hot Coffee</h1>
 
-            {searchResult
-              .filter((item) => item.type === 'cold')
-              .slice(0, 3)
-              .map((item) => (
-                <Card {...item} key={item.id} />
-              ))}
+            {hotItems.length > 0 ? (
+              hotItems.map((item) => <Card key={item.id} {...item} />)
+            ) : (
+              <p className="text-muted">No hot coffee found ☕</p>
+            )}
+          </div>
+
+          {/* --- Cold Coffee --- */}
+          <div className="col-lg-6">
+            <h1 className="mb-5">Cold Coffee</h1>
+
+            {coldItems.length > 0 ? (
+              coldItems.map((item) => <Card key={item.id} {...item} />)
+            ) : (
+              <p className="text-muted">No cold coffee found ❄️</p>
+            )}
           </div>
         </div>
       </div>
