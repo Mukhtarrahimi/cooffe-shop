5956df84bba99a2c766724f97824ddd7d3777765
= <mukhtarrahimi.dev@gmail.com>
2025-10-30 08:10:52 +0430

    product title link

--- components/modules/Card/Card.js
+++ components/modules/Card/Card.js
@@ -1,15 +1,18 @@
+import Link from 'next/link';
 import React from 'react';
 
-function Card({ title, desc, price, img }) {
+function Card({ title, desc, price, img, id }) {
   return (
-    <div className="row align-items-center mb-5">
-      <div className="col-4 col-sm-3">
-        <img className="w-100 rounded-circle mb-3 mb-sm-0" src={img} alt="" />
-        <h5 className="menu-price">${price}</h5>
+    <div class="row align-items-center mb-5">
+      <div class="col-4 col-sm-3">
+        <img class="w-100 rounded-circle mb-3 mb-sm-0" src={img} alt="" />
+        <h5 class="menu-price">${price}</h5>
       </div>
-      <div className="col-8 col-sm-9">
-        <h4>{title}</h4>
-        <p className="m-0">{desc}</p>
+      <div class="col-8 col-sm-9">
+        <Link href={`/products/${id}`}>
+          <h4>{title}</h4>
+        </Link>
+        <p class="m-0">{desc}</p>
       </div>
     </div>
   );
